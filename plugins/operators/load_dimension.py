4c433e5dec0d70105d7be057d8e2d168aa3df764
Esteban L. Gómez - UAH <esteban.ligero@edu.uah.es>
2025-08-22 22:27:37 +0200

    Operators - LoadDimensionOperator - !dev

--- plugins/operators/load_dimension.py
+++ plugins/operators/load_dimension.py
@@ -8,15 +8,31 @@ class LoadDimensionOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 table="",
+                 sql_query="",
+                 truncate_table=False,
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.table = table
+        self.sql_query = sql_query
+        self.truncate_table = truncate_table
 
     def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
+        self.log.info(f"Loading data into dimension table {self.table}")
+        
+        redshift_hook = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        
+        if self.truncate_table:
+            self.log.info(
+                f"Truncating dimension table {self.table} before loading"
+            )
+            redshift_hook.run(f"TRUNCATE TABLE {self.table}")
+            
+        formatted_sql = f"INSERT INTO {self.table} {self.sql_query}"
+        
+        self.log.info(f"Executing INSERT statement: {formatted_sql}")
+        redshift_hook.run(formatted_sql)
+        self.log.info(f"Successfully loaded dimension table {self.table}")
